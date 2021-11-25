3d3f24625ba467f10dcda6364748ebab02a8c8b8
boraprae <72434691+boraprae@users.noreply.github.com>
2021-11-25 23:26:33 +0700

    ้sqlite helper

--- onen/lib/dictionary.dart
+++ onen/lib/dictionary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:onen/constantColor.dart';
+import 'package:onen/sqlite_helper.dart';
 
 class Dictionary extends StatefulWidget {
   const Dictionary({Key? key}) : super(key: key);
@@ -10,79 +11,83 @@ class Dictionary extends StatefulWidget {
 
 class _DictionaryState extends State<Dictionary> {
   TextEditingController searchController = TextEditingController();
+  final SQLiteHelper _helper = SQLiteHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    //connect DB
+    _helper.openDB();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 24, 0),
-            child: IconButton(
-              onPressed: () {
-                print('Exit');
-              },
-              icon: Icon(
-                Icons.exit_to_app_outlined,
-                color: Colors.black,
-                size: 30,
-              ),
-            ),
-          )
-        ],
-        title: Padding(
-          padding: const EdgeInsets.fromLTRB(24, 32, 0, 0),
-          child: Row(
-            children: [
-              const Text(
-                'ONEN',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: blueSky,
-                ),
-              ),
-              const Text(
-                ' DICT',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
       body: Container(
         color: Colors.white,
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: SizedBox(
-                  height: 50,
-                  child: TextField(
-                    controller: searchController,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search_outlined),
-                      hintText: 'Search word or phrase',
-                      hintStyle: TextStyle(fontSize: 12.0, color: Colors.grey),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey, width: 0.5),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.grey, width: 0.5),
-                      ),
+              padding: const EdgeInsets.fromLTRB(24, 50, 24, 0),
+              child: Row(
+                children: [
+                  const Text(
+                    'ONEN',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: blueSky,
+                    ),
+                  ),
+                  const Text(
+                    ' DICT',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {
+                      print('Exit');
+                    },
+                    icon: Icon(
+                      Icons.exit_to_app_outlined,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
+              child: SizedBox(
+                height: 50,
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search_outlined),
+                    hintText: 'Search word or phrase',
+                    hintStyle: TextStyle(fontSize: 12.0, color: Colors.grey),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey, width: 0.5),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey, width: 0.5),
                     ),
                   ),
                 ),
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _helper.getData();
+              },
+              child: const Text('Get data'),
             ),
           ],
         ),
---
+++ onen/lib/sqlite_helper.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SQLiteHelper {
+  late Database _db;
+  String dbFile = 'lexitron.sqlite';
+
+  void openDB() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, dbFile);
+
+    //check db existed?
+    bool exists = await databaseExists(path);
+
+    if (!exists) {
+      print('No have DB');
+      //no db
+      //copy db from asset folder
+      try {
+        Directory(dirname(path)).create(recursive: true);
+      } catch (e) {
+        print("Error: cannot create folder");
+      }
+
+      //copy fb to device's folder
+      //open file and read
+      ByteData data = await rootBundle.load(join('assets/db', dbFile));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes);
+      //wirte a file
+      await File(path).writeAsBytes(bytes, flush: true);
+      print('Copy DB success');
+    } else {
+      print('Use existsing DB');
+    }
+    
+    //open DB
+    _db = await openDatabase(path, readOnly: true, singleInstance: true);
+  }
+
+  void getData() async {
+    List result = await _db
+        .rawQuery('SELECT tentry FROM eng2th WHERE esearch = ?', ['abacus']);
+    if (result.isEmpty) {
+      print('Not found');
+    } else {
+      print(result);
+    }
+  }
+
+  void closeDB() async {
+    await _db.close();
+    print('DB closed');
+  }
+}
