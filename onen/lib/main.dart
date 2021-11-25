27c83d20064beef1c550fca333acaf524ce529ac
kann1er <sivakait@gmail.com>
2021-11-25 23:24:18 +0700

    test merge 2

--- onen/lib/main.dart
+++ onen/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 // import 'package:onen/Minigame.dart';
-import 'package:onen/Quiz.dart';
-import 'package:onen/Replaypage.dart';
+import 'package:onen/miniGame/Quiz.dart';
+import 'package:onen/miniGame/Replaypage.dart';
+import 'package:onen/miniGame/Minigame.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Replay(),
+    home: Quiz(),
   ));
 }
---
+++ onen/lib/miniGame/Quiz.dart
@@ -0,0 +1,309 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class Quiz extends StatefulWidget {
+  const Quiz({Key? key}) : super(key: key);
+
+  @override
+  _QuizState createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  int time = 20;
+  String question = 'to put off or reschedule for a later time; to delay';
+  String trueAnswer = 'Postpone';
+
+  String choice1 = 'Heartily';
+  String choice2 = 'Postpone';
+  String choice3 = 'Feeble';
+  String choice4 = 'Domestic';
+
+  void checkAnswer(String choice) {
+    if (choice == trueAnswer) {
+      // give user a point
+      // show result
+      goToNextQuestion();
+    } else {
+
+      // show result
+      goToNextQuestion();
+    }
+  }
+
+  void goToNextQuestion() {
+
+  }
+
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        time--;
+        if (time == 0) {
+          timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(
+        //   title: const Text('MINI GAME'),
+        // ),
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                Text(
+                  'MINI',
+                  style: TextStyle(
+                    color: Color(0xFF3FC0DF),
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'GAME',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              children: const [
+                Text(
+                  'Select the word which match with this meaning',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 120,
+              width: 360,
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Color(0xFF2A9DF4),
+                    Color(0xFF7CC0F1),
+                    Color(0xFFD7E8F4),
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Text(
+                      '${question}', //! text can chang 
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {    //* ---------------------Button answer------------------------
+                    
+                  },
+                  child: Container(
+                    height: 90,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFD0EFFF),
+                      border: Border.all(
+                        color: const Color(0xFFD0EFFF),
+                        width: 8,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${choice1}', //! text can chang 
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF2A9DF4),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){   //* ---------------------Button answer------------------------
+                    
+
+                  },
+                  child: Container(
+                    height: 90,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFA6FFD1),
+                      border: Border.all(
+                        color: const Color(0xFFA6FFD1),
+                        width: 8,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${choice2}', //! can chang text
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF23BC8E),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {//* ---------------------Button answer------------------------
+                    
+
+                  },
+                  child: Container(
+                    height: 90,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFEEFB0),
+                      border: Border.all(
+                        color: const Color(0xFFFEEFB0),
+                        width: 8,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${choice3}', //! can chang text
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF2C112),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (){//* ---------------------Button answer------------------------
+                    
+
+                  },
+                  child: Container(
+                    height: 90,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFFAF94),
+                      border: Border.all(
+                        color: const Color(0xFFFFAF94),
+                        width: 8,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${choice4}', //! can chang text
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFDA3131),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Text(
+                  'Time remaining',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset(
+                  'assets/images/clock.png',
+                  width: 20.0,
+                  height: 20.0,
+                ),
+                const SizedBox(
+                  width: 5.0,
+                ),
+                Text(
+                  '${time}S',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
