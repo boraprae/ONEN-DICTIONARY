fd5e453e72f041acc3ffa661fe5c279fc91e0db7
ARoma278 <matephat15@gmail.com>
2021-11-25 21:16:56 +0700

    UI replay page

--- onen/lib/main.dart
+++ onen/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:onen/Minigame.dart';
+// import 'package:onen/Minigame.dart';
 import 'package:onen/Quiz.dart';
+import 'package:onen/Replaypage.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Quiz(),
+    home: Replay(),
   ));
 }
